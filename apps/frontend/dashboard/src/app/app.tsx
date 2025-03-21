82fcd4988e7653f3596dd4895014aa33a49bc6c4
Burak Citaci <burak.citaci@enpal.de>
2025-03-21 07:08:28 +0100

    tailwindcss 4 init

--- apps/frontend/dashboard/src/app/app.tsx
+++ apps/frontend/dashboard/src/app/app.tsx
@@ -1,9 +1,54 @@
-import { Button } from '@iotsphere/ui-lib/components/ui/button';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+  Label,
+  Input,
+  Button,
+} from '@iot-sphere/ui-lib';
 
+/**
+ * App
+ *
+ * The main app component. This component is responsible for rendering the
+ * entire app, including the navigation and routes.
+ *
+ * The app is currently very basic and just renders a simple form and a few
+ * links to navigate between pages.
+ *
+ * @returns The app component.
+ */
 export function App() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <Button variant="outline">Button</Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>Create project</CardTitle>
+          <CardDescription>
+            Deploy your new project in one-click.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" placeholder="Name of your project" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label>Hello World</Label>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Cancel</Button>
+          <Button>Deploy</Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
--- apps/frontend/dashboard/vite.config.mts
+++ apps/frontend/dashboard/vite.config.mts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(() => ({
   root: __dirname,
@@ -14,7 +15,7 @@ export default defineConfig(() => ({
     port: 4300,
     host: 'localhost',
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
@@ -29,7 +30,7 @@ export default defineConfig(() => ({
   },
   resolve: {
     alias: {
-      '@iotsphere/ui-lib': path.resolve(__dirname, '../library/src/'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   test: {
